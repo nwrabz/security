60d75707b20d0b53e62479cb77166e9843094477
Nasasiira Rajjab <nwrabz@gmail.com>
2026-02-06 19:00:34 +0300

    UserDetailsService

---
+++ src/main/java/org/wrabz/sec/config/ProjectConfig.java
@@ -0,0 +1,30 @@
+package org.wrabz.sec.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var userDetailsService = new InMemoryUserDetailsManager();
+
+        var user = User.withUsername("user")
+                .password("12345")
+                .build();
+        userDetailsService.createUser(user);
+
+        return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
