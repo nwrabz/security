44dad38ae5a77eb8719d412d03c88aa4819cb16a
Nasasiira Rajjab <nwrabz@gmail.com>
2026-02-07 14:29:43 +0300

    jpa for users and using userDetails

---
+++ src/main/java/org/wrabz/sec/model/User.java
@@ -0,0 +1,19 @@
+package org.wrabz.sec.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity(name = "USERS")
+@Data
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String username;
+    private String password;
+    private String authority;
+}
