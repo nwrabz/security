4a3f7bd3619233f556a1b5625efd33b93df27313
Nasasiira Rajjab <nwrabz@gmail.com>
2026-02-07 11:52:26 +0300

    configuring

--- src/main/java/org/wrabz/sec/config/ProjectConfig.java
+++ src/main/java/org/wrabz/sec/config/ProjectConfig.java
@@ -2,21 +2,25 @@ package org.wrabz.sec.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
 
     @Bean
-    public UserDetailsService userDetailsService() {
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
         var userDetailsService = new InMemoryUserDetailsManager();
 
         var user = User.withUsername("user")
-                .password("12345")
+                .password(encoder.encode("password"))
+                .authorities("read")
                 .build();
         userDetailsService.createUser(user);
 
@@ -26,5 +30,16 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(auth ->
+                        auth
+                                .anyRequest()
+                                .permitAll()
+                );
+        return http.build();
     }
 }
