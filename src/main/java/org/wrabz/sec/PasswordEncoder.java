a86942138fcfc5d135fcad580fc77e53d4bb853c
Nasasiira Rajjab <nwrabz@gmail.com>
2026-02-08 11:41:26 +0300

    PasswordEncoder contract

---
+++ src/main/java/org/wrabz/sec/PasswordEncoder.java
@@ -0,0 +1,9 @@
+package org.wrabz.sec;
+
+public interface PasswordEncoder {
+    String encode(CharSequence rawPassword);
+    boolean matches(CharSequence rawPassword, String encodedPassword);
+    default boolean upgradeEncoding(String encodedPassword) {
+        return false;
+    }
+}
