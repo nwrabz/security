c98f9542c01a07fdcf0fdd691a09ec15dfe77ccb
Nasasiira Rajjab <nwrabz@gmail.com>
2026-02-10 12:56:39 +0300

    The SecurityContext interface

--- src/main/java/org/wrabz/sec/SecurityContext.java
+++ src/main/java/org/wrabz/sec/SecurityContext.java
@@ -1,4 +1,10 @@
 package org.wrabz.sec;
 
-public interface SecurityContext {
+import org.springframework.security.core.Authentication;
+
+import java.io.Serializable;
+
+public interface SecurityContext extends Serializable {
+    Authentication getAuthentication();
+    void setAuthentication(Authentication authentication);
 }
