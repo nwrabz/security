d7522307f281b806b899ce0ffa930f1e458f7cb9
Nasasiira Rajjab <nwrabz@gmail.com>
2026-02-11 23:30:43 +0300

    Changing the authentication method to a form-based login

--- src/main/java/org/wrabz/sec/Config.java
+++ src/main/java/org/wrabz/sec/Config.java
@@ -1,6 +1,7 @@
 package org.wrabz.sec;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.wrabz.sec.controller.CustomEntryPoint;
@@ -10,10 +11,7 @@ public class Config {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .httpBasic(c ->{
-                    c.realmName("OTHER");
-                    c.authenticationEntryPoint(new CustomEntryPoint());
-                })
+                .formLogin(Customizer.withDefaults())
                 .authorizeHttpRequests(a -> a.anyRequest().authenticated());
         return http.build();
 
