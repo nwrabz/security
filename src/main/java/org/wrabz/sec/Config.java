3caa9c7fccba06cee9be3061bfc4f28e25629898
Nasasiira Rajjab <nwrabz@gmail.com>
2026-02-12 00:21:38 +0300

    Implementing an AuthenticationFailureHandler

--- src/main/java/org/wrabz/sec/Config.java
+++ src/main/java/org/wrabz/sec/Config.java
@@ -1,11 +1,12 @@
 package org.wrabz.sec;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 public class Config {
+
+    private CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
