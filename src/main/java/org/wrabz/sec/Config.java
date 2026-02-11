037343cb4eef52b8b345f051bf7cd1994d9eef66
Nasasiira Rajjab <nwrabz@gmail.com>
2026-02-12 00:27:59 +0300

    Registering the handler objects in the configuration class

--- src/main/java/org/wrabz/sec/Config.java
+++ src/main/java/org/wrabz/sec/Config.java
@@ -1,19 +1,25 @@
 package org.wrabz.sec;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 public class Config {
 
-    private CustomAuthenticationSuccessHandler customAuthenticationSuccessHandler;
+    @Autowired
+    private CustomAuthenticationSuccessHandler authenticationSuccessHandler;
+
+    @Autowired
+    private CustomAuthenticationFailureHandler authenticationFailureHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .formLogin(form -> form
-                        .defaultSuccessUrl("/home", true)
-                )
+                        .successHandler(authenticationSuccessHandler)
+                        .failureHandler(authenticationFailureHandler))
+
 
                 .authorizeHttpRequests(a -> a.anyRequest().authenticated());
         return http.build();
