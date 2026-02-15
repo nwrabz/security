eaa2a5cb1e47ee61c63939c05d45c102b61afa81
Nasasiira Rajjab <nwrabz@gmail.com>
2026-02-15 23:12:16 +0300

    Making all the endpoints accessible for everyone without authentication

--- src/main/java/org/wrabz/sec/config/ProjectConfig.java
+++ src/main/java/org/wrabz/sec/config/ProjectConfig.java
@@ -2,9 +2,15 @@ package org.wrabz.sec.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
@@ -27,5 +33,18 @@ public class ProjectConfig {
         userDetailsService.createUser(user2);
 
         return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .httpBasic(Customizer.withDefaults())
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().permitAll());
     }
 }
