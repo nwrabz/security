ba4c97ad0d6f69f73d10462e28933686bd5192e2
Nasasiira Rajjab <nwrabz@gmail.com>
2026-02-18 11:02:41 +0300

    Implementing the the filter to replace the our Basic filter

--- src/main/java/org/wrabz/sec/config/ProjectConfig.java
+++ src/main/java/org/wrabz/sec/config/ProjectConfig.java
@@ -1,5 +1,6 @@
 package org.wrabz.sec.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -13,38 +14,22 @@ import org.springframework.security.web.authentication.www.BasicAuthenticationFi
 import org.springframework.security.web.util.matcher.RegexRequestMatcher;
 import org.wrabz.sec.filter.AuthenticationLoggingFilter;
 import org.wrabz.sec.filter.RequestValidationFilter;
+import org.wrabz.sec.filter.StaticKeyAuthenticationFilter;
 
 @Configuration
 public class ProjectConfig {
 
-    @Bean
-    public UserDetailsService userDetailsService() {
+    private final StaticKeyAuthenticationFilter filter;
 
-        var userDetailsService = new InMemoryUserDetailsManager();
-
-        var user1 = User.withUsername("user1")
-                .password("password")
-                .roles("ADMIN")
-                .build();
-        var user2 = User.withUsername("user2")
-                .password("password")
-                .authorities("read", "premium")
-                .build();
-
-        userDetailsService.createUser(user1);
-        userDetailsService.createUser(user2);
-
-        return userDetailsService;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+    public ProjectConfig(StaticKeyAuthenticationFilter filter) {
+        this.filter = filter;
     }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .addFilterAt(filter,//Add the filter at the position of Basic Authentication filter
+                        BasicAuthenticationFilter.class)
                 .addFilterBefore(
                         new RequestValidationFilter(),
                         BasicAuthenticationFilter.class
--- src/main/java/org/wrabz/sec/filter/StaticKeyAuthenticationFilter.java
+++ src/main/java/org/wrabz/sec/filter/StaticKeyAuthenticationFilter.java
@@ -4,9 +4,11 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 public class StaticKeyAuthenticationFilter implements Filter {
 
     @Value("${authorization.key}")
